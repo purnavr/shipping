91098c2127f6949802c475cd03062952dc815da8
purna <purnaraomba@gmail.com>
2024-09-14 16:55:06 +0530

    Roboshop Update

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,4 @@
-java -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
+source /params/params.txt
+java -jar /app/shipping.jar
+
+#java -XX:MinRAMPercentage=70 -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
