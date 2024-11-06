4972efbb3d5e82074e0a4a3d8ac4ed7f0d8e64bb
purna <purnaraomba@gmail.com>
2024-11-06 17:37:05 +0530

    Roboshop Update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,3 +6,4 @@ env.sonar_extra_opts = "-Dsonar.java.binaries=./target"
 ci()
 
 //sh 'sonar-scanner -Dsonar.host.url=http://172.31.21.138:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true '
+
